906b9ccde83da710c5494fd754ebccfc6611fd0f
yemmyharry <yemmyharry@gmail.com>
2024-06-22 10:31:19 +0100

    feat: add vite config

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { configDefaults } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+    css: false,
+    exclude: [...configDefaults.exclude, 'node_modules']
+  }
+});
